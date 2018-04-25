0308c63da0d59bf33dbcff948a6941ee7b1d3d41
moth <moth@gionee.com>
2018-04-25 21:41:05 +0800

    客服修改

--- app/src/main/java/com/smona/app/xiniueval/ui/home/fragment/KefuFragment.java
+++ app/src/main/java/com/smona/app/xiniueval/ui/home/fragment/KefuFragment.java
@@ -19,24 +19,4 @@ public class KefuFragment extends ContentFragment {
     protected int getLayoutId() {
         return R.layout.fragment_kefu;
     }
-
-    protected void init(View root) {
-        WebView webView = (WebView)root.findViewById(R.id.kefu_webview);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        webSettings.setUseWideViewPort(true);//关键点
-        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-        webSettings.setDisplayZoomControls(false);
-        webSettings.setJavaScriptEnabled(true); // 设置支持javascript脚本
-        webSettings.setAllowFileAccess(true); // 允许访问文件
-        webSettings.setBuiltInZoomControls(true); // 设置显示缩放按钮
-        webSettings.setSupportZoom(true); // 支持缩放
-        webSettings.setLoadWithOverviewMode(true);
-
-        UserItem userItem = new UserItem();
-        userItem.readSelf(getContext());
-        String url = HttpDelegator.getInstance().getCacheKey(UrlConstants.KEFU, "?clientUser=" + userItem.mId);
-        CarLog.d("motianhu", "url=" +url);
-        webView.loadUrl(url);
-    }
 }
